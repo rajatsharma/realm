c5b6f68c26414b269723653d5e56e403b1b690e4
Rajat Sharma <lunasunkaiser@gmail.com>
2022-02-17 15:36:35 +0530

    Use string formatting, add function addInstallAbbr for fish shell

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,28 @@
 module Main where
 
+import Soothsayer ((***))
 import System.Directory (doesPathExist, getCurrentDirectory)
 import System.Environment (getArgs, getEnv, setEnv)
 
-addToPath :: String -> String -> IO ()
-addToPath "fish" cwd = putStrLn $ "set -g fish_user_paths " ++ cwd ++ "/node_modules/.bin $fish_user_paths"
-addToPath _ cwd = putStrLn $ "path+=" ++ cwd ++ "/node_modules/.bin"
+addToPath :: String -> String -> String
+addToPath "fish" cwd = "set -g fish_user_paths {0}/node_modules/.bin $fish_user_paths" *** [cwd]
+addToPath _ cwd = "path+={0}/node_modules/.bin" *** [cwd]
+
+addInstallAbbr :: String -> String -> String
+addInstallAbbr "fish" packageManager = "abbr -a -g ins {0} install" *** [packageManager]
+addInstallAbbr _ _ = ""
 
 main :: IO ()
 main = do
   cwd <- getCurrentDirectory
   args <- getArgs
   isNode <- doesPathExist $ cwd ++ "/package.json"
+  isYarn <- doesPathExist $ cwd ++ "/yarn.lock"
+  isPnpm <- doesPathExist $ cwd ++ "/pnpm-lock.yaml"
+  let packageManager
+        | isYarn = "yarn"
+        | isPnpm = "pnpm"
+        | otherwise = "npm"
   path <- getEnv "PATH"
   let shell = head args
-  if isNode then addToPath shell cwd else pure ()
+  if isNode then putStrLn $ unlines [addToPath shell cwd, addInstallAbbr shell packageManager] else pure ()
